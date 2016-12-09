ba4aeb98d62a77a9ae96c7e56432c54e15aa5094
aydinkahriman <aydinkahriman@gmail.com>
2016-12-09 16:26:30 +0300

    Add project files.

---
+++ Deneme/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Deneme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.CurrentUICulture = CultureInfo.InvariantCulture;
+            string s = "43.23";
+            double d;
+            MessageBox.Show(double.TryParse(s, NumberStyles.AllowDecimalPoint, CultureInfo.InvariantCulture, out d).ToString());
+        }
+    }
+}
